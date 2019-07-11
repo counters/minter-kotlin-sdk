f5bbb3a42a0d2284562b28f9d3cdd9d4f6c0480b
Webcounters <webcounters@gmail.com>
2019-07-11 21:25:45 +0300

    Add files
    Signed-off-by: Webcounters <webcounters@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,10 +17,18 @@ class Main {
               println(status)
               val node = minterApi.getNodeRaw("Mp01cc99ae5a349ecaeef187dcbb12816bf2b3d8eae80f654034b21213aa445b2c")
               println(node)
-              val coin = minterApi.getCoin("BTCSECURE")
-            println(coin)
-            val block = minterApi.getBlockRaw(973584)
-            println(block)
+            val coin = minterApi.getCoin("ROBOT")
+            println(coin) //  Coin(id=null, symbol=ROBOT, length=5, name=Coin for robots, creater=null, crr=80, ...)
+            val wallet = minterApi.getAddress("Mxcd633fd8ec1b0a181627dfd72f9ba25e93f0c899")
+            println(wallet) // Wallet(id=null, address=Mxcd633fd8ec1b0a181627dfd72f9ba25e93f0c899, count_txs=17, balance={BTCSECURE=5.3534213964374E-5, BIP=4245.51470327139, SATOSHI=888.8888})
+            val block = minterApi.getBlockRaw(2)
+            println(block) /*
+            BlockRaw(height=2, time=2019-05-15T18:05:02.395+03:00, num_txs=3, total_txs=3, reward=333.0, size=4310, proposer=Mp...,
+                transaction=[TransactionRaw(hash=Mt..., height=2, type=5, from=Mx..., to=null, node=, stake=15000000000000000000000000,
+                coin=BTCSECURE, amount=1.5E7, gas_price=1, commission=null, payload=false, gas=100000, gascoin=BIP)},
+                "payload":"","gas":"10000","from":"Mx...","service_data":"","gas_coin":"BIP","type":6,"raw_tx":"******","nonce":"1",
+                "hash":"Mt.....","tags":{"tx.type":"06","tx.from":"****"}}])
+                */
             val estimateCoinSell = minterApi.estimateCoinSell("BIP", 1.0, "BIP")
             println(estimateCoinSell) // EstimateCoinSell(willGet=21.908222878076707, commission=0.1)
             val estimateCoinBuy = minterApi.estimateCoinBuy("BIP", 1.0, "BTCSECURE")
