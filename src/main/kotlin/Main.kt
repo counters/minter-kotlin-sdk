2dc63bbe43be3c66140ce27bdc11c688f5bd7441
Webcounters <webcounters@gmail.com>
2020-07-04 20:34:34 +0300

    add Examples in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,31 +8,28 @@ class Main {
 
             val minterApi = MinterApi("http://127.0.0.1:8841", 30.0)
 
-            val events = minterApi.getEventsRaw(14)
+            val events = minterApi.getEventsRaw(37920)
             println(events)
 
             val status = minterApi.getStatus()
             println(status) // Status(height=977154, datetime=2019-07-11T21:26:22.119+03:00, network=minter-mainnet-1)
-              val node = minterApi.getNodeRaw("Mp01cc99ae5a349ecaeef187dcbb12816bf2b3d8eae80f654034b21213aa445b2c")
+              val node = minterApi.getNodeRaw("Mp3b6e2632cd0c91ef96ea4f4a16f554ff1b4dd41324fd421a0161947c50603b9b")
             println(node) // NodeRaw(reward=Mx0a76...dd711, owner=Mx5ebe...7799, pub_key=Mp01cc...5b2c, commission=10, crblock=4)
             val coin = minterApi.getCoin("ROBOT")
             println(coin) //  Coin(id=null, symbol=ROBOT, length=5, name=Coin for robots, creater=null, crr=80, ...)
-            val wallet = minterApi.getAddress("Mxcd633fd8ec1b0a181627dfd72f9ba25e93f0c899")
-            println(wallet) // Wallet(id=null, address=Mxcd633fd8ec1b0a181627dfd72f9ba25e93f0c899, count_txs=17, balance={BTCSECURE=5.3534213964374E-5, BIP=4245.51470327139, SATOSHI=888.8888})
-            val block = minterApi.getBlockRaw(268)
-            println(block) /*
-            BlockRaw(height=2, time=2019-05-15T18:05:02.395+03:00, num_txs=3, total_txs=3, reward=333.0, size=4310, proposer=Mp...,
-                transaction=[TransactionRaw(hash=Mt..., height=2, type=5, from=Mx..., to=null, node=, stake=15000000000000000000000000,
-                coin=BTCSECURE, amount=1.5E7, gas_price=1, commission=null, payload=false, gas=100000, gascoin=BIP)},
-                "payload":"","gas":"10000","from":"Mx...","service_data":"","gas_coin":"BIP","type":6,"raw_tx":"******","nonce":"1",
-                "hash":"Mt.....","tags":{"tx.type":"06","tx.from":"****"}}])
-                */
+            val wallet = minterApi.getAddress("Mxabcd4613b06bc5a78412cb55a09bdf3f94790321")
+            println(wallet) // Wallet(id=null, address=Mxabcd4613b06bc5a78412cb55a09bdf3f94790321, count_txs=17, balance={UPLOAD=5.3534213964374E-5, BIP=4245.51470327139, SATOSHI=888.8888})
+            val block = minterApi.getBlockRaw(1532991)
+            println(block)
+            val transaction = minterApi.getTransactionRaw("Mt083bdf87f22ccfe62d55951eaa6b7a8d7618f83f214df6b0e516858bc3a837ef")
+            println(transaction)
+
             val estimateCoinSell = minterApi.estimateCoinSell("ROBOT", 1.0, "BIP")
             println(estimateCoinSell) // EstimateCoinSell(willGet=21.908222878076707, commission=0.1)
-            val estimateCoinBuy = minterApi.estimateCoinBuy("BIP", 1.0, "BTCSECURE")
+            val estimateCoinBuy = minterApi.estimateCoinBuy("BIP", 1.0, "UPLOAD")
             println(estimateCoinBuy) // EstimateCoinBuy(willPay=0.04564504450213553, commission=0.1)
 
-            val newNode = minterApi.getNode("Mp01cc99ae5a349ecaeef187dcbb12816bf2b3d8eae80f654034b21213aa445b2c", 0, {
+            val newNode = minterApi.getNode("Mp3b6e2632cd0c91ef96ea4f4a16f554ff1b4dd41324fd421a0161947c50603b9b", 0, {
                 val reward = minterApi.getAddress(it)
 //                println(reward)
                 0
@@ -43,7 +40,7 @@ class Main {
                     owner.balance!!.forEach { coin, amount ->
                         if (coin != "BIP") {
                             val priceInBip = minterApi.estimateCoinSell(coin, amount, "BIP")!!.willGet
-                            println("$amount $coin = $priceInBip BIP") // 100.00006004578127 BTCSECURE = 4.564748939706455 BIP
+                            println("$amount $coin = $priceInBip BIP") // 100.00006004578127 UPLOAD = 4.564748939706455 BIP
                         } else {
                             println("$amount $coin")
                         }
