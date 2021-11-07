2d4ca98a149cd8bb3b71633f1dbfeb8068b8baf5
Webcounters <webcounters@gmail.com>
2021-11-07 08:47:19 +0300

    upgrade tests

--- src/test/kotlin/counters/minter/sdk/MinterApi/MinterApiTest.kt
+++ src/test/kotlin/counters/minter/sdk/MinterApi/MinterApiTest.kt
@@ -2,21 +2,52 @@ package counters.minter.sdk.MinterApi
 
 import counters.minter.sdk.Minter.Enum.QueryTags
 import counters.minter.sdk.Minter.Enum.TransactionTypes
+import counters.minter.sdk.Minter.Minter
+import counters.minter.sdk.Minter.MinterRaw
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class MinterApiTest {
 
-    private val minterApi = MinterApi("http://127.0.0.1:8843/v2", 30.0)
-    val testAddress = "Mx0000000000000000000000000000000000000000"
+    private val minterApi = MinterApi("http://127.0.0.1:8843/v2", 300.0)
+    private val _testAddress = "Mx0000000000000000000000000000000000000000"
+
+//    private var status: Minter.Status? = null
+//    private var address: String? = null
+
+    data class Value4test(
+        var init: Boolean = false,
+        var status: Minter.Status? = null,
+        var address: String? = null,
+        var hash: String? = null,
+//        var status: Minter.Status? = null,
+    )
+    private val value4test = Value4test()
+
+    init {
+//        if (!value4test.init) loadValuesForMethod()
+
+
+    }
+
+    private fun loadValuesForMethod(httpMethod: HttpMethod? = null){
+        val method = if (httpMethod==null) HttpMethod.values() else arrayOf(httpMethod)
+        method.forEach {
+            if (it== HttpMethod.STATUS) value4test.status = minterApi.getStatus()
+            if (it== HttpMethod.ADDRESS) value4test.address = _getTransactionsRaw(TransactionTypes.TypeSend)!!.first().from
+            if (it== HttpMethod.TRANSACTION) value4test.hash = _getTransactionsRaw(TransactionTypes.TypeSend)!!.first().hash
+        }
+        println(value4test)
+    }
 
     @Test
-    fun getAddress() {
-
-        val result = minterApi.getAddress(testAddress)
+    fun getAddress(/*address: String? = null*/) {
+//        val testAddress = address ?: value4test.address
+        val address: String = if (value4test.address!=null ) value4test.address!! else this._getTransactionsRaw(TransactionTypes.TypeSend)!!.first().from
+        val result = minterApi.getAddress(address)
         if (result != null) {
-            assertEquals(testAddress, result.address)
+            assertEquals(address, result.address)
         } else {
             assert(false)
         }
@@ -24,6 +55,7 @@ internal class MinterApiTest {
 
     @Test
     fun getStatus() {
+        assertNotEquals(null, minterApi.getStatus())
         assertNotEquals(null, minterApi.getStatus())
     }
 
@@ -35,15 +67,61 @@ internal class MinterApiTest {
     }
 
     @Test
-    fun getTransactionsRaw() {
-        val queryMap = mutableMapOf(
-            QueryTags.TagsTxType to TransactionTypes.TypeMultiSend.toHex(),
-        )
-        minterApi.getTransactionsRaw(queryMap, 1,2)?.let {
-            if (it.count()>1) assert(true)
+    fun getTransactionRaw(/*transaction: String? = null*/) {
+//        val hash = transaction ?: value4test.hash!!
+        val hash: String = if (value4test.hash!=null ) value4test.hash!! else this._getTransactionsRaw(TransactionTypes.TypeSend)!!.first().hash
+
+        minterApi.getTransactionRaw(hash)?.let {
+            if (it.hash == hash  ) assert(true)
         } ?: run{
             assert(false)
         }
+//        assert(false)
+    }
+
+//    @Test
+    fun getTransactionsRaw(type: TransactionTypes?= null): MinterRaw.TransactionRaw? {
+        val transactionType = type ?: TransactionTypes.TypeSend
+        val queryMap = mutableMapOf(
+            QueryTags.TagsTxType to transactionType.toHex(),
+        )
+        println(queryMap)
+        minterApi.getTransactionsRaw(queryMap, 1,1)?.let {
+            if (it.isNotEmpty()) {
+                assert(true)
+                value4test.hash= it.first().hash
+                return it.first()
+            }
+        } ?: run{
+            assert(false)
+        }
+        return null
+    }
+    fun _getTransactionsRaw(type: TransactionTypes?= null): List<MinterRaw.TransactionRaw>? {
+        val transactionType = type ?: TransactionTypes.TypeSend
+        val queryMap = mutableMapOf(
+            QueryTags.TagsTxType to transactionType.toHex(),
+        )
+        minterApi.getTransactionsRaw(queryMap, 1,1)?.let {
+            if (it.isNotEmpty()) {
+                assert(true)
+//                value4test.hash= it.first().hash
+                return it
+            }
+        } ?: run{
+            assert(false)
+        }
+        return null
+    }
+
+
+//    @Test
+    fun getAllMethod(){
+        HttpMethod.values().forEach {
+            if (it== HttpMethod.STATUS) return getStatus()
+        }
+        assert(true, { "@TODO" }) // TODO("add method")
+//        assert(false, { "ggg" })
     }
 
 }
