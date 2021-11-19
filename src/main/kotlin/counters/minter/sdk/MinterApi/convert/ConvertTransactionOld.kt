f1e582a6d2030c8722828de2f628d842d0f6fa8c
Webcounters <webcounters@gmail.com>
2021-11-19 23:52:03 +0300

    tmp add

---
+++ src/main/kotlin/counters/minter/sdk/MinterApi/convert/ConvertTransactionOld.kt
@@ -0,0 +1,36 @@
+package counters.minter.sdk.MinterApi.convert
+
+import com.google.common.io.BaseEncoding.base64
+import counters.minter.grpc.client.TransactionResponse
+import counters.minter.sdk.Minter.CoinObjClass
+import counters.minter.sdk.Minter.MinterRaw
+
+@Deprecated(level = DeprecationLevel.WARNING, message = "Deprecated")
+interface ConvertTransactionOld {
+
+    fun getTransaction(transaction: TransactionResponse): MinterRaw.TransactionRaw {
+
+        return MinterRaw.TransactionRaw(
+            hash = transaction.hash,
+            height = transaction.height,
+            type = transaction.type.toInt(),
+            from = transaction.from,
+            to = transaction.hash,
+            node = transaction.hash,
+            stake = "10000000",
+            coin = CoinObjClass.CoinObj(-1,"NULL"),
+            coin2 = CoinObjClass.CoinObj(-1,"NULL"),
+            amount = -1.0,
+            gas_price = transaction.gasCoin.id.toInt(),
+            commission = -1.0,
+            payload = false,
+            gas = transaction.gas.toInt(),
+            gascoin = CoinObjClass.CoinObj(transaction.gasCoin.id,transaction.gasCoin.symbol),
+            optDouble = -1.0,
+            optString = "NULL",
+            optList = null,
+            base64Payload = base64().encode(transaction.payload.toByteArray()) ,
+
+        )
+    }
+}
