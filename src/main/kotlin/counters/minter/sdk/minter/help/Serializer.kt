ec73d761e33321ef015e3bb51fbcf0f486dbb112
Webcounters <webcounters@gmail.com>
2022-05-04 22:28:26 +0300

    up Serializer.kt

--- src/main/kotlin/counters/minter/sdk/minter/help/Serializer.kt
+++ src/main/kotlin/counters/minter/sdk/minter/help/Serializer.kt
@@ -2,6 +2,7 @@ package counters.minter.sdk.minter.help
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import counters.minter.sdk.minter.MinterRaw.EventRaw
 import counters.minter.sdk.minter.models.TransactionRaw
 
@@ -14,20 +15,14 @@ class Serializer {
     fun transaction(jsonObject: JsonObject) = serializerTransaction.decode(jsonObject)
     fun transaction(json: String) = serializerTransaction.decode(json)
 
+
     fun event(eventRaw: EventRaw) = serializerEvent.encode(eventRaw)
     fun event(eventsRaw: List<EventRaw>) = serializerEvent.encode(eventsRaw)
 
-/*    fun event(json: JSONObject): EventRaw? {
-        TODO("Not yet implemented")
-    }*/
-
     fun event(jsonArray: JsonArray) = serializerEvent.decode(jsonArray)
-
-
     fun event(jsonObject: JsonObject) = serializerEvent.decode(jsonObject)
 
+    fun event(json: String) = serializerEvent.decode(json)
+    fun events(json: String) = serializerEvent.decode(JsonParser.parseString(json).asJsonArray)
 
-/*    fun events(height: List<MinterRaw.EventRaw>): JsonObject? {
-        TODO("Not yet implemented")
-    }*/
 }
