8c6519223b3cbf56d9a7fff945f32523372ebf05
Webcounters <webcounters@gmail.com>
2022-05-16 10:10:16 +0300

    added pools of orders and pools to QueryTags to search for transactions

--- src/main/kotlin/counters/minter/sdk/minter/enum/QueryTags.kt
+++ src/main/kotlin/counters/minter/sdk/minter/enum/QueryTags.kt
@@ -4,4 +4,6 @@ enum class QueryTags(val str: String) {
     TagsTxFrom("tags.tx.from"),
     TagsTxTo("tags.tx.to"),
     TagsTxType("tags.tx.type"),
+    TagsTxOrderId("tags.tx.order_id"),
+    TagsTxPoolId("tags.tx.pool_id"),
 }
