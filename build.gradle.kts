b3e222a5c608538ce099cb79350b2e580edc3543
webcounters <webcounters@gmail.com>
2022-11-11 22:58:12 +0300

    up version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 group = "counters"
-version = "2.4.6"
+version = "2.4.7"
 
-val grpcKotlinVersion = "1.2.1"
+val grpcKotlinVersion = "1.3.0"
 val coroutinesVersion = "1.6.1"
 
 
@@ -31,24 +31,24 @@ dependencies {
 
     implementation(group = "org.json", name = "json", version = "20200518")
     testImplementation("org.slf4j", "slf4j-simple", "1.7.26")
-    implementation("joda-time:joda-time:2.10.14")
+    implementation("joda-time:joda-time:2.12.1")
 
     implementation(fileTree("libs"))
 
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10")
 
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
 
     implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
 
-    implementation("io.grpc:grpc-netty-shaded:1.46.0")
-    implementation("io.grpc:grpc-protobuf:1.46.0")
-    implementation("io.grpc:grpc-stub:1.46.0")
+    implementation("io.grpc:grpc-netty-shaded:1.50.2")
+    implementation("io.grpc:grpc-protobuf:1.50.2")
+    implementation("io.grpc:grpc-stub:1.50.2")
 
-    implementation("com.google.protobuf:protobuf-java:3.20.3")
+    implementation("com.google.protobuf:protobuf-java:3.21.9")
     implementation("com.google.protobuf:protobuf-kotlin:3.20.3")
     implementation(kotlin("stdlib-jdk8"))
 
